65700e64ac5627d168d5417674b560575eade0fa
dswhaley <whaleyds22@gcc.edu>
2025-08-19 11:50:48 -0400

    Added logging

--- resources/scripts/websocket.sh
+++ resources/scripts/websocket.sh
@@ -5,4 +5,4 @@ export FRAPPE_SITE=${FRAPPE_SITE_NAME_HEADER}
 export REDIS_QUEUE=redis://redis:6379
 export REDIS_SOCKETIO=redis://redis:6379
 
-node /home/frappe/frappe-bench/apps/frappe/socketio.js
+node /home/frappe/frappe-bench/apps/frappe/socketio.js >> /tmp/websocket.log 2>&1
